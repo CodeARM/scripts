117f5d65b40b4fc7da60a0aa0904db59f2fbf8c6
CodeARM <20066673+CodeARM@users.noreply.github.com>
2025-11-21 11:37:11 -0500

    make a file with file list

---
+++ listmyfilestoday.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# make a log of the files in the current dir 
+# needs a way to catch when this doesn't work
+
+dateandtime=$(date '+%Y-%m-%d_%H-%M-%S')
+filelist=$(ls -al)
+printf "$filelist" > "list-of-files-$dateandtime.txt"
+echo For $dateandtime, "I've added a list of files to your current directory"

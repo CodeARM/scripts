030de085fc18cb636daaf64efe827ce5c3c21f21
CodeARM <20066673+CodeARM@users.noreply.github.com>
2025-11-30 10:25:04 -0500

    updated CPU usage to be more basic output

--- simplebashhealthreporter.sh
+++ simplebashhealthreporter.sh
@@ -11,7 +11,8 @@ echo ""
 echo "CPU Usage:"
 
 # Average CPU load over the last 1, 5, and 15 minutes
-uptime | awk -F'load average: ' '{ print "Load Average: " $2 }'
+uptime # | awk -F'load average: ' '{ print "Load Average: " $2 }'
+
 # Current CPU utilization snapshot
 top -bn1 | grep "CPU(s)" | awk '{print "CPU Usage: " ($2 + $4) "%"}'
 echo

78284845fd0cedd22561e217f152216cb3aef892
CodeARM <20066673+CodeARM@users.noreply.github.com>
2025-12-01 12:07:11 -0500

    works but is very basic, cleaned up spacing a bit as well

--- simplebashhealthreporter.sh
+++ simplebashhealthreporter.sh
@@ -9,29 +9,23 @@ echo ""
 
 # CPU Usage
 echo "CPU Usage:"
+uptime 
+# cpu_idle=$(top -l 1 | awk '/CPU usage/ {print $7}' | sed 's/%//') | echo "CPU Idle:  ${cpu_idle}%"
+echo ""
 
-# Average CPU load over the last 1, 5, and 15 minutes
-uptime # | awk -F'load average: ' '{ print "Load Average: " $2 }'
-
-# Current CPU utilization snapshot
-top -bn1 | grep "CPU(s)" | awk '{print "CPU Usage: " ($2 + $4) "%"}'
-echo
-
-# Memory Usage - Free and used memory
-# free -h | awk 'NR==2{print "Memory Usage: Used " $3 " / Total " $2 " (" $3/$2*100 "%)"}'
+# Memory Usage
 echo "Memory Usage:"
-free -h | awk 'NR==2{printf "Used: %s / Total:
-    %s (%.2f%%)\n", $3, $2, $3/$2*100}'
-echo
+mem_total_bytes=$(sysctl -n hw.memsize) 
+echo $mem_total_bytes
+echo ""
+
 # Disk Usage
 echo "Disk Usage:"
-df -h --total | grep 'total' | awk '{print "Used: " $
-3 " / Total: " $2 " (" $5 ")"}'
-echo
+df -h
+echo ""
+
 # Network Status
-echo "Network Status:"
-# Display active network interfaces and their statuses
-ip -br addr show | awk '{print $1 ": " $3}'
-echo
+# echo "Network Status:"
+
 
 echo "============="

9538ab769324414c0479cad432f9a85e8b9d8ded
CodeARM <20066673+CodeARM@users.noreply.github.com>
2025-12-10 16:05:37 -0500

    it works sort of- need this on record

--- greppyfind.py
+++ greppyfind.py
@@ -1,35 +1,31 @@
+import sys
 import os
 
 # THIS DOES NOT WORK BUT I WANT IT TRACKED 
 
 # since grep looks in files
 # and find finds filenames
-# I want to find this string inside a file or in a filename
+# I want to have a way to do both grep for strings in files and find strings in filenames
+# I want to also want grep or find separately
+# I want to use parameters to make printing things easy and having one change for the searches
 
-# I want to have a way to do both grep for things in files and find strings in filenames
-# I want to also want grep or find
+# What are the steps
 
-string_to_find="test"
-
-# loop through all the files in this directory
-
-for file in files:
-  if filename contains $string_to_find
-    print("$filename contains $string_to_find")
-  else:
-    print("No filenames contain $string_to_find")
-
-# find filenames with 'test' in them
-
-
-
+# indicate where to grep or find (is it as simple as "."???)
+# loop through all the files in this directory (how do i do this? make a fake array ??)
+# find filenames with 'test' in them (use "in")
 # print out $filename has 'test' in the filename
 # parameterize 'test'
-{
-    find . -type f -name "*test*.sh" 
-    grep -rl --include="*.sh" "test" . 
-} 
+# in the final, print out "this file has in the body of"
+# echo "this file has in the name" or "... body"
+# could be ideal to say for each file if it has either or both
 
-# | echo "this file has in the body of"
-# | echo "this file has in the name"
+string_to_find="test"
+files=os.listdir('.')
 
+
+for filename in files:
+  if string_to_find in filename:
+    print(f"{filename} contains '{string_to_find}'")
+  else:
+    print(f"No filenames contain '{string_to_find}'")

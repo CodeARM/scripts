b6905d60d57301f262c51cee7af2d91f7c549082
CodeARM <20066673+CodeARM@users.noreply.github.com>
2025-12-10 15:25:03 -0500

    committing broken greppy

--- greppyfind.py
+++ greppyfind.py
@@ -1,22 +1,30 @@
-import 
+import os
+
+# THIS DOES NOT WORK BUT I WANT IT TRACKED 
 
 # since grep looks in files
 # and find finds filenames
 # I want to find this string inside a file or in a filename
 
-# to make this reusable, I want to have a way to do both grep and find searches
-# "test" can be replaced with the string you are looking for
+# I want to have a way to do both grep for things in files and find strings in filenames
+# I want to also want grep or find
 
-// loop through all the files in this directory
+string_to_find="test"
 
-for file in files
+# loop through all the files in this directory
 
-// find filenames with 'test' in them
+for file in files:
+  if filename contains $string_to_find
+    print("$filename contains $string_to_find")
+  else:
+    print("No filenames contain $string_to_find")
+
+# find filenames with 'test' in them
 
 
 
-// print out $filename has 'test' in the filename
-// parameterize 'test'
+# print out $filename has 'test' in the filename
+# parameterize 'test'
 {
     find . -type f -name "*test*.sh" 
     grep -rl --include="*.sh" "test" . 

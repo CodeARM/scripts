03f0552d59e29fd3fc30a3c6955b6e3e29c120a0
CodeARM <20066673+CodeARM@users.noreply.github.com>
2025-12-14 14:59:18 -0500

    broke and fixed this one, just typing too quickly in vim

--- fancyfind.sh
+++ fancyfind.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-# since grep looks in files
-# and find finds filenames
+# since grep looks in files and find finds filenames
 # I want to find this string inside a file or in a filename
-
-# to make this reusable, I want to have a way to do both grep and / or find searches 
-# where I comment out a line I'm not using or have both
-
 # "test" can be replaced with the string I am looking for 
+
 maxargs=1
+
 if [ $# -ne $maxargs ]
 then 
     echo "You need to say the string you want to look for \"$0 string-name\"" # $0 is the name of the file in case I change the filename this will update
     exit
 fi
+
 {
     findings=$(find . -type f -name "*$1*.sh" -exec echo "{} contains $1 in the filename" \;)
-    greppings=$(grep -rl --include="*.sh" "$1" . | sed "s/$/ contains $1 inside \n /")
+    greppings=$(grep -rl --include="*.sh" "$1" . | sed "s/$/ contains $1 inside /")
     echo $findings
     echo $greppings
 } 
